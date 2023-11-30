ddf9b4dd64dd39425832ec91186acdc8c01eb2ee
Alex Merida Castillo <mcastilloalex@gmail.com>
2023-11-29 19:19:43 -0700

    updated README// add css// updated both html files// added js code and api info to script.js

--- README.md
+++ README.md
@@ -7,12 +7,14 @@ Weather_Dashboard
 
 #Mock_UP
 
-![alt Weather_Dashboard](./assets/image/Weather_Dashboard.jpeg)
+![alt Weather_Dashboard](./assets/image/Weather_Dashboard.png)
 
 #Usage
 
 
 #Credits
+
+WeatherAPI-Link="https://openweathermap.org/api/one-call-3"
 
 Github-Deploy="https://ameridacas.github.io/Whats_The_Weather_App/"
 
---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function searchWeather() {
+    // Placeholder function for searching weather by city name
+    alert('Searching weather for: ' + document.getElementById('search-input').value);
+}
+//api:"017bca9d42017769b4b46e88e4a81cce", keyname: Whats_the_WeatherAPI
+//api:"c8724acd65684f09fa89b3306a1a04d3", keyname: Default
+//`https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`
+
