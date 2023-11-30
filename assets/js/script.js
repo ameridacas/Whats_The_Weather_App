7285ae8ba6927b237f12bdeca03006890592a333
Alex Merida Castillo <mcastilloalex@gmail.com>
2023-11-29 19:27:13 -0700

    updated script.js with some variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+
+//api:"017bca9d42017769b4b46e88e4a81cce", keyname: Whats_the_WeatherAPI
+//api:"c8724acd65684f09fa89b3306a1a04d3", keyname: Default
+//`https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`
+//`https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={017bca9d42017769b4b46e88e4a81cce}`
+//`https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={c8724acd65684f09fa89b3306a1a04d3}`
+// Variables
+var APIKey = "017bca9d42017769b4b46e88e4a81cce";
+var DefaultAPIKey = "c8724acd65684f09fa89b3306a1a04d3";
+var APIName= "Whats_the_WeatherAPI";
+var DefaultAPIName= "Default";
 function searchWeather() {
     // Placeholder function for searching weather by city name
     alert('Searching weather for: ' + document.getElementById('search-input').value);
