9355825d92f7fafc62ffc78cd9807b4a2fe29a34
Alex Merida Castillo <mcastilloalex@gmail.com>
2023-11-29 22:58:25 -0700

    updated the script.js and index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,39 @@
 //`https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={c8724acd65684f09fa89b3306a1a04d3}`
 
 // Variables
-var APIKey = "017bca9d42017769b4b46e88e4a81cce";
-var DefaultAPIKey = "c8724acd65684f09fa89b3306a1a04d3";
-var APIName = "Whats_the_WeatherAPI";
-var DefaultAPIName = "Default";
+var apiKey = "017bca9d42017769b4b46e88e4a81cce";
+var defaultApiKey = "c8724acd65684f09fa89b3306a1a04d3";
+var apiName = "Whats_the_WeatherAPI";
+var defaultApiName = "Default";
 var searchInput = document.getElementById('search-input');
 var weatherDataKey = 'weatherData';
+var searchHistoryKey = 'searchHistory';
+
+function getWeatherData(city, apiKey) {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`)
+        .then(function (response) {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(function (data) {
+            // Store the API data with the city name as the key
+            localStorage.setItem(city.toLowerCase(), JSON.stringify(data));
+
+            // Retrieve and update the search history
+            var searchHistory = JSON.parse(localStorage.getItem(searchHistoryKey)) || [];
+            if (!searchHistory.includes(city)) {
+                searchHistory.push(city);
+                localStorage.setItem(searchHistoryKey, JSON.stringify(searchHistory));
+            }
+
+            alert('Weather data and search history updated in local storage.');
+        })
+        .catch(function (error) {
+            console.error('Error fetching weather data:', error);
+        });
+}
 
 function searchWeather() {
     var cityName = searchInput.value;
@@ -22,28 +49,9 @@ function searchWeather() {
     }
 
     // Choose the API key based on the selected API
-    var selectedAPIKey = APIName === "Whats_the_WeatherAPI" ? APIKey : DefaultAPIKey;
-    // API URL
-    var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${selectedAPIKey}`;
+    var selectedApiKey = apiName === "Whats_the_WeatherAPI" ? apiKey : defaultApiKey;
 
-    // Fetch data from the API
-    fetch(apiUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            // Store the weather data in local storage
-            localStorage.setItem(weatherDataKey, JSON.stringify(data));
-
-            // Placeholder: You can update the UI or navigate to another page here
-            alert('Weather data fetched and stored in local storage.');
-        })
-        .catch(error => {
-            console.error('Error fetching weather data:', error);
-        });
+    getWeatherData(cityName, selectedApiKey);
 }
 
 // function to check and parse the stored data in local storage
@@ -52,9 +60,13 @@ function displayStoredWeatherData() {
 
     if (storedData) {
         var parsedData = JSON.parse(storedData);
-       
         console.log(parsedData);
     } else {
         console.log('No weather data found in local storage.');
     }
 }
+
+// Call this function to display stored weather data
+displayStoredWeatherData();
+
+
